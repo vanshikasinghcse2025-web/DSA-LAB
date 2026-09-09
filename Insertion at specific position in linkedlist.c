adcfeb17f5ac8189642738c7a490b0f7dbfa778b
Vanshika22 <vanshika.singh.cse.2025@miet.ac.in>
2026-09-09 10:17:56 +0530

    Create Insertion at specific position in linkedlist.c

---
+++ Insertion at specific position in linkedlist.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
